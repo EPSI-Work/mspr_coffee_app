bcb5a80c53e658862c28591420b7d69e671a2b9f
Lecat Baptiste <60200125+BaptisteLecat@users.noreply.github.com>
2023-03-08 23:39:30 +0100

    Build the UI of the scan waiting page

--- lib/presentation/feature/auth/screen/scan_qr_code_screen.dart
+++ lib/presentation/feature/auth/screen/scan_qr_code_screen.dart
@@ -5,10 +5,55 @@ class ScanQrCodeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('ScanQrCodeScreen'),
+    return Scaffold(
+        body: SafeArea(
+            child: Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: [
+          Expanded(
+            flex: 2,
+            child: Image.asset(
+              'assets/gifs/qrcode.gif',
+              width: 256,
+              height: 256,
+            ),
+          ),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        'Scan the QR Code',
+                        style: Theme.of(context).textTheme.headlineMedium,
+                      ),
+                      Text(
+                        'Please finish the login process by simply scanning the QR code sent to your email',
+                        style: Theme.of(context).textTheme.bodyLarge,
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ),
+                Flexible(
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Scan QR Code',
+                        style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                              color: Colors.white,
+                            ),
+                      )),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
-    );
+    )));
   }
 }
